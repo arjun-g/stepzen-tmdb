7c35afabb3691ed2b1e7561238bfb0675ffa8d52
Techands Dev <dev@techands.dev>
2022-07-18 10:30:04 +0530

    fix cors

--- app/functions/api.js
+++ app/functions/api.js
@@ -1,44 +1,29 @@
 export async function onRequestPost({ request, env }) {
-    const newRequest = new Request("https://lakebluff.stepzen.net/api/tmdb/__graphql", request);
-    newRequest.headers.set("Authorization", `Apikey ${env.STEPZEN_API_KEY}`);
-    return await fetch(newRequest);
+  const newRequest = new Request(
+    "https://lakebluff.stepzen.net/api/tmdb/__graphql",
+    request
+  );
+  newRequest.headers.set("Authorization", `Apikey ${env.STEPZEN_API_KEY}`);
+  return await fetch(newRequest);
 }
 
 const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-    'Access-Control-Max-Age': '86400',
-  };
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+};
 
 export function onRequestOptions(request) {
-    // Make sure the necessary headers are present
-    // for this to be a valid pre-flight request
-    let headers = request.headers;
-    if (
-      headers.get('Origin') !== null &&
-      headers.get('Access-Control-Request-Method') !== null &&
-      headers.get('Access-Control-Request-Headers') !== null
-    ) {
-      // Handle CORS pre-flight request.
-      // If you want to check or reject the requested method + headers
-      // you can do that here.
-      let respHeaders = {
-        ...corsHeaders,
-        // Allow all future content Request headers to go back to browser
-        // such as Authorization (Bearer) or X-Client-Name-Version
-        'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
-      };
-  
-      return new Response(null, {
-        headers: respHeaders,
-      });
-    } else {
-      // Handle standard OPTIONS request.
-      // If you want to allow other HTTP Methods, you can do that here.
-      return new Response(null, {
-        headers: {
-          Allow: 'GET, HEAD, POST, OPTIONS',
-        },
-      });
-    }
-  }
+  let respHeaders = {
+    ...corsHeaders,
+    // Allow all future content Request headers to go back to browser
+    // such as Authorization (Bearer) or X-Client-Name-Version
+    "Access-Control-Allow-Headers": request.headers.get(
+      "Access-Control-Request-Headers"
+    ),
+  };
+
+  return new Response(null, {
+    headers: respHeaders,
+  });
+}
